e37fd430d33aaee1c2c784130711ddee9040e521
suolabi <suolabi@163.com>
2020-10-04 21:28:48 +0800

    seconed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <Header></Header>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
-  name: '',
+  name: 'App',
+  components:{
+    Header,
+    Footer
+  }
 }
 </script>
 
---
+++ src/components/TypeNav/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'TypeNav',
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
